abe3630fee16ba73abe31722b0ef7d5f61d81863
Ngai Ying Ying <yyngai2.gniy@gmail.com>
2016-05-14 08:38:29 +0000

    Add Comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514031049) do
+ActiveRecord::Schema.define(version: 20160514081744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
